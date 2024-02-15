78bee4e146d015a040ace5a26c27cf69913232fb
Ashtonaut <ashtonautica@gmail.com>
2024-02-15 17:20:46 +0000

    Add functionality to add new books

--- script.js
+++ script.js
@@ -1,12 +1,17 @@
 const myLibrary = [];
 const tbody = document.querySelector("tbody");
+const formButton = document.querySelector("form button");
+const form = document.querySelector("form");
 
-function Book(title) {
+function Book(title, author, pages, read) {
     this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
 };
 
-function addBookToLibrary(title) {
-    myLibrary.push(new Book(title));
+function addBookToLibrary(title, author, pages, read) {
+    myLibrary.push(new Book(title, author, pages, read));
 };
 
 function clearTable() {
@@ -19,13 +24,25 @@ function displayBooks() {
     clearTable();
     myLibrary.forEach(book => {
         const tr = document.createElement("tr");
-        const title = document.createElement("td");
-        title.textContent = book.title;
-        tr.appendChild(title);
+        for (const value of Object.values(book)) {
+            const entry = document.createElement("td");
+            if (value === true || value === false) {
+                entry.textContent = value.toString().charAt(0).toUpperCase() + value.toString().slice(1);
+            } else {
+                entry.textContent = value;
+            }
+            tr.appendChild(entry);
+        }
         tbody.appendChild(tr);
-    });
+    })
 };
 
-addBookToLibrary("The Hobbit");
-addBookToLibrary("Treasure Island");
+formButton.addEventListener("click", e => {
+    e.preventDefault();
+    addBookToLibrary(form.title.value, form.author.value, form.pages.value, Boolean(form.read.value));
+    displayBooks();
+})
+
+addBookToLibrary("The Hobbit", "J.R.R Tolkien", 310, true);
+addBookToLibrary("Treasure Island", "Robert Louis Stevenson", 209, false);
 displayBooks();
