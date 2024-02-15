c2fb7342392810033e81230f1df06c8422815fd0
Ashtonaut <ashtonautica@gmail.com>
2024-02-15 15:48:05 +0000

    Add displayBooks and clearTable functions

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const myLibrary = [];
+const tbody = document.querySelector("tbody");
 
 function Book(title) {
     this.title = title;
@@ -6,4 +7,21 @@ function Book(title) {
 
 function addBookToLibrary(title) {
     myLibrary.push(new Book(title));
+};
+
+function clearTable() {
+    while (tbody.firstChild) {
+        tbody.removeChild(tbody.lastChild);
+    };
+};
+
+function displayBooks() {
+    clearTable();
+    myLibrary.forEach(book => {
+        const tr = document.createElement("tr");
+        const title = document.createElement("td");
+        title.textContent = book.title;
+        tr.appendChild(title);
+        tbody.appendChild(tr);
+    });
 };
